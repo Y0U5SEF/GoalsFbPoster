1a05c423725abf9f73fee332f68f74b48eb0c9a1
Y0U5SEF <116289965+Y0U5SEF@users.noreply.github.com>
2024-08-09 20:59:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function postVideo(page, videoFile, caption) {
 }
 
 async function main() {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext({
     permissions: ["notifications"],
   });
